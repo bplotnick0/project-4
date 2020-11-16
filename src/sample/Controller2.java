a5386fdc2b33f74a3862c340ec8045ca28c68c1a
msherb5 <msherbs5@gmail.com>
2020-11-15 22:03:17 -0500

    Controller2

--- src/sample/Controller2.java
+++ src/sample/Controller2.java
@@ -1,4 +1,67 @@
 package sample;
 
-public class Controller2 {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+
+
+//import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller2 implements Initializable {
+
+    @FXML
+    RadioButton sameOrd, remOrd, cleOrd, savOrd;
+    @FXML
+    ListView ordList;
+    @FXML
+    TextArea totDisplay;
+    Order order = new Order();
+
+
+    @FXML
+    public void display(OrderLine orderline){
+        ordList.getItems().add(orderline);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+
+
+
+
+
+    public void exportFile(ActionEvent event){
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open Target File for the Export");
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", ".txt"),
+                new FileChooser.ExtensionFilter("All Files", ".*"));
+        Stage stage = new Stage();
+        File targeFile = chooser.showSaveDialog(stage);
+
+        try {
+            FileWriter out = new FileWriter(targeFile);
+            out.write(Order.toString());
+            out.close();
+
+        } catch (IOException e) {
+            ordDisplay.appendText(e.getMessage());
+        }
+
+    }
+
+
 }
